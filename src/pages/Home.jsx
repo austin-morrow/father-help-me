08d694b29844516bc309516377873e86f7a80187
Austin Morrow <morrow.austin5@gmail.com>
2023-03-14 22:24:41 -0600

    icons on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SlSocialInstagram, SlSocialFacebook, SlSocialSpotify } from "react-icons/sl";
 import { Link } from 'react-router-dom'
 import IMG from '../assets/images/IMG_3792.jpg'
 import Folder from '../assets/images/folder.png'
@@ -8,10 +9,10 @@ function Home() {
   return (
     <>
     <div className='pt-4 pl-4'>home</div>
-    <div className="flex justify-center text-2xl sm:text-4xl">Father, Help Me</div>
+    <div className="flex justify-center text-2xl lg:pb-0 pb-8 sm:text-4xl">Father, Help Me</div>
     <div>
     <div className="relative flex justify-center">
-      <img className="w-full sm:w-auto sm:h-[400px]  lg:h-[500px] sm:p-4 pb-8 lg:p-12" src={IMG} alt="Father help me band" />
+      <img className="w-full sm:w-auto sm:h-[400px]  lg:h-[400px] sm:p-4 pb-8 lg:p-12" src={IMG} alt="Father help me band" />
     </div>
 
 <div className="flex justify-center">
@@ -20,6 +21,13 @@ function Home() {
         <div className="flex justify-center text-xs lg:text-base">joy in pain?</div>
      </div>
 </div>
+   <div className='fixed bottom-4 left-0 right-0'>
+    <div className='object-none object-bottom flex justify-center gap-4 text-gray-500'>
+        <div className='hover:text-black'><a href='https://www.instagram.com/fatherhelpmeband/' rel="noreferrer" target="_blank"> <SlSocialInstagram /></a></div>
+        <div className='hover:text-black'> <a href='https://www.facebook.com/FatherHelpMeBand' rel="noreferrer" target="_blank"><SlSocialFacebook /></a></div> 
+      <div className='hover:text-black'><a href='https://open.spotify.com/artist/5hLg85exsPamjHRb12tHPW'  rel="noreferrer" target="_blank"><SlSocialSpotify /></a></div>
+    </div>
+    </div>
  </div>
     </>
   );
